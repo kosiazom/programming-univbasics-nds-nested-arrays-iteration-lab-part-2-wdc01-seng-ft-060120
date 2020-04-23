476e7d6a874f0365b466172488442b683e0a5342
Kosi Akporji <kosiazom@gmail.com>
2020-04-23 18:30:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,6 @@ small_num_array = [ ]
 row_index = 0 
  while row_index < src.length do 
    element_index = 0 
-   small_number = src.min
    while element_index < src[row_index].length do
      if src[row_index][element_index] == src[row_index].min
        small_number = src[row_index].min
