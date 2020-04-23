fb809cf9478bb30efc8cee2f7bc20e42401e93f9
Kosi Akporji <kosiazom@gmail.com>
2020-04-23 17:51:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,9 +5,12 @@ row_index = 0
  while row_index < src.length do 
    element_index = 0 
    while element_index < src[row_index].length do
-     
+     if src[row_index][element_index][0] == src.min
+       small_num_array << src[row_index][element_index]
+     end 
+    element_index +=1 
+  end
+  row_index +=1 
   
-  
-  
-
 end
+small_num_array
